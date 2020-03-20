440f9675788a8ca6f6b7bb2a030dd5c0b26b6098
EC2 Default User <civichann@gmail.com>
2020-03-20 08:53:25 +0000

    メインカラムの追加

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,17 @@
 @extends('layouts.app')
-
 @section('content')
-<p>d</p>
+<div class="row justify-content-center">
+	<div class="col-md-4">
+		<div class="cord mb50">
+			<div class="image-wrapper"><img class="book-image" src="{{ asset('images/dummy.png') }}"></div>
+			<h3 class="h3 book-title">タイトル</h3>
+				<p class="description">
+					レビュー本文
+				</p>
+				<a href="" class="btn btn-secondary detail-btn">詳細を読む</a>
+		</div>
+	</div>
+</div>
 @endsection 
 
 
