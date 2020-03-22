d6b198bf3209d35503981e55fc55acb7fac4ca93
EC2 Default User <civichann@gmail.com>
2020-03-22 04:44:15 +0000

    ミドルウェアグループを指定

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,10 @@ Route::get('/' , 'ReviewController@index')->name('index');
 
 Auth::routes();
 
-Route::get('/review','ReviewController@create')->name('index');
+Route::group(['middleware' => 'auth'], function () {
+
+Route::get('/review','ReviewController@create')->name('create');
+    
+});
+
 Route::get('/home', 'HomeController@index')->name('home');
