098afda4c7aa8b0e36de9a4166eccc3ebf2370bc
EC2 Default User <civichann@gmail.com>
2020-03-23 13:58:48 +0000

    ReviewControlle修正

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -3,11 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Review;
 
 class ReviewController extends Controller
 {
     public function index()
     {
         return view('index');
+    }
+    public function create()
+    {
+        return view('review');
+    }
+    public function store(Request $request)
+    {
+        $post = $request->all();
+        $request->file('image')->store('/public/images');
+        $data = ['user_id' => \Auth::id(), 'title' => $post['title'], 'body' => $post['body'],'image' => $request->file('image')->hashName()];
+        
+        Review::insert($data);
+
+        return redirect('/');
     }
 }
